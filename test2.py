4215413ffe237b133e20e428ec925612ff677136
Erik Wallin <erikwallin86@gmail.com>
2023-01-07 16:24:49 +0100

    Add draft of test2, to download data

---
+++ test2.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, request
+import sys
+import os
+import json
+
+app = Flask(__name__)
+
+
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+
+@app.route('/data/', methods=['POST', 'GET'])
+def data():
+    if request.method == 'GET':
+        return f"The URL /data is accessed directly. Try going to '/form' to submit form"
+    if request.method == 'POST':
+        form_data = request.form
+        print(f"form_data:{form_data}")
+        for k, v in form_data.items():
+            print(f"(k, v):{(k, v)}")
+
+        sys.path.append("MoonBoard")
+        from MoonBoard.fetch_logbook import request_data
+
+        # Get username and password
+        username = form_data['username']
+        password = form_data['password']
+        # Define cache filename
+        filename = f"logbook_{username}.json"
+
+        # Download logbook, or fetch cached
+        if not os.path.isfile(filename):
+            # Download data and save
+            logbook_data = request_data(access_kwargs={
+                'username': username,
+                'password': password})
+            with open(filename, 'w') as json_file:
+                json.dump(logbook_data, json_file, indent=4)
+        else:
+            # Load cached data
+            with open(filename) as json_file:
+                logbook_data = json.load(json_file)
+
+        print(f"logbook_data:{logbook_data}")
+
+        return render_template('data.html', form_data=form_data)
+
+
+app.run(host='localhost', port=5000)
